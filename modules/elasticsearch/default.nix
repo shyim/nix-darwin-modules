20fe2aa6de5f014b3f5cef8757e411a0b1f586b7
Shyim <6224096+shyim@users.noreply.github.com>
2022-05-20 14:46:55 +0200

    Update default.nix

--- modules/elasticsearch/default.nix
+++ modules/elasticsearch/default.nix
@@ -10,8 +10,6 @@ let
     network.host: ${cfg.listenAddress}
     cluster.name: ${cfg.cluster_name}
     ${lib.optionalString cfg.single_node "discovery.type: single-node"}
-    ${lib.optionalString (cfg.single_node && es7)
-    "gateway.auto_import_dangling_indices: true"}
     http.port: ${toString cfg.port}
     transport.port: ${toString cfg.tcp_port}
     ${cfg.extraConf}
