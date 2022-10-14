d5a1c5afb3949b73b4ac778057ac6eb25dc0615c
Soner Sayakci <github@shyim.de>
2022-10-14 15:21:02 +0000

    add input

--- flake.nix
+++ flake.nix
@@ -1,6 +1,8 @@
 {
   description = "A collection of darwin modules";
 
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
   outputs = { self, nixpkgs }: {
     darwinModules = rec {
         modules = import ./modules;
